22ecd21bc1ba30d553e5dcc5997e756dd77d8328
fleth <fleth@users.noreply.github.com>
2019-04-21 08:06:53 +0000

    changed usage

--- setup_sample.sh
+++ setup_sample.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-current=`pwd`
-echo $current
-git submodule update -i
-git submodule foreach git pull origin master
-
 mkdir ~/workspace
 rm ~/workspace/stock_data
-ln -s $current/data ~/workspace/stock_data
-
-sh setup/install.sh
-sh setup/setup.sh
+ln -s `pwd`/data ~/workspace/stock_data
